6aaccbaab2913b45224854dd47d2cf1201290fad
steph-crown <emmanuelstephen024@gmail.com>
2022-06-04 06:49:43 +0100

    Add username field to schema

--- lib/pento/accounts/user.ex
+++ lib/pento/accounts/user.ex
@@ -5,6 +5,7 @@ defmodule Pento.Accounts.User do
   schema "users" do
     field :email, :string
     field :password, :string, virtual: true, redact: true
+    field :username, :string
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
 
